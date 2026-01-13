1d1dc6366de676135e33fbd372ef6e35171530aa
Diya Rao <125043488+Diya-004@users.noreply.github.com>
2026-01-13 17:45:57 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,17 @@
 import fastf1
-import pandas as pd
 
-fastf1.Cache.enable_cache("cache")
+def load_race_data():
+    fastf1.Cache.enable_cache("cache")
 
-def load_race_data(year=2023, race_name="British Grand Prix"):
-    session = fastf1.get_session(year, race_name, "R")
+    year = 2023
+    race_name = "British Grand Prix"
+
+    session = fastf1.get_session(
+        year,
+        race_name,
+        "R",
+        backend="ergast"
+    )
+
     session.load()
-
-    laps = session.laps
-    return laps
+    return session.laps
